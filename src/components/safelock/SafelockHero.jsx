988aa42c60d52f84e562bfa74b606319f785a1a6
midestic <badmususman50@gmail.com>
2025-07-06 18:25:08 +0100

    safelock mobile screen

--- src/components/safelock/SafelockHero.jsx
+++ src/components/safelock/SafelockHero.jsx
@@ -1,53 +1,46 @@
-import React from 'react'
+import React from "react";
 
 const SafelockHero = () => {
   return (
- <div>
-       <div className='flex justify-between items-center bg-[#2295F2] px-[50px] pt-[160px] '>
-      <section className="max-w-[34em]">
-     <h1 className="text-white font-bold text-[50px] py-2">Safelock</h1>
+    <div>
+      <div
+        className="flex justify-between items-center bg-[#2295F2] px-[50px] pt-[160px] max-md:flex-col  
+      max-md:gap-y-[50px]"
+      >
+        <section className="max-w-[34em] max-md:max-w-[100%] max-md:mx-auto  max-md:flex max-md:flex-col max-md:items-center">
+          <h1 className="text-white font-bold text-[50px] py-2">Safelock</h1>
 
-     <main className="bg-white max-w-[180px] rounded-[20px] flex items-center  py-2.5 px-[12px]" >
-        <img src="" alt="" />
-        <p className="text-[#2295F2] 9ext-[20px] py-0.5">Fixed Savings</p>
-     </main>
+          <main className="bg-white max-w-[180px] rounded-[20px] flex justify-center items-center  py-2.5 px-[12px] max-md:w-[70%] ">
+            <img src="" alt="" />
+            <p className="text-[#2295F2] 9ext-[20px] py-0.5 text-center">
+              Fixed Savings
+            </p>
+          </main>
 
-     <p className="text-white text-[20px] leading-[32px] font-[400] py-4">
- Lock funds away for a fixed period of time to avoid spending temptation. Earn up to 20% p.a. paid upfront.
+          <p className="text-white text-[20px] leading-[32px] font-[400] py-4 max-md:text-center ">
+            Lock funds away for a fixed period of time to avoid spending
+            temptation. Earn up to 20% p.a. paid upfront.
+          </p>
 
-</p>
+          <main className="flex">
+            <div>
+              <img src="" alt="" />
+              <p className="text-white"> Get on iPhone</p>
+            </div>
 
+            <div>
+              <img src="" alt="" />
+              <p className="text-white"> Get on Android</p>
+            </div>
+          </main>
+        </section>
 
-
-<main className="flex">
-<div>
-    <img src="" alt="" />
-    <p className="text-white"> Get on iPhone</p>
-</div>
-
-<div>
-    <img src="" alt="" />
-    <p className="text-white"> Get on Android</p>
-</div>
-
-
-</main>
-
-      </section>
-
-
-
-      <section className='w-[500px]'>
-        <img src="../../../public/SL 22 Big.png" alt=""/>
-      </section>
-
-
+        <section className="w-[500px] max-md:w-[70%] max-md:mx-auto">
+          <img src="../../../public/SL 22 Big.png" alt="" />
+        </section>
+      </div>
     </div>
+  );
+};
 
-
-
-    </div>
-  )
-}
-
-export default SafelockHero
+export default SafelockHero;
